24ab99e1b341601c3e5f198b356a8ac17aaf0a4a
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-12-22 17:51:17 +0200

    Add global variables

---
+++ client/src/assets/styles/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1d1d1d",
+    },
+    secondary: {
+      main: "#f8f1ed",
+    },
+  },
+  typography: {
+    fontFamily: "Poppins, Arial",
+  },
+});
