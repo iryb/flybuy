cd4a9716fa6f7e3817ea3ae939113a0c9443b88b
Iryna <iryna.burianovata@gmail.com>
2023-03-14 13:01:52 +0200

    Redux setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./assets/styles/theme";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./store/store";
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
