5447f908fd3014818ccb016ab1ea9b525b135983
Iryna <iryna.burianovata@gmail.com>
2023-03-14 17:08:25 +0200

    Add path aliases

---
+++ client/craco.config.cjs
@@ -0,0 +1,15 @@
+const path = require("path");
+const CracoAlias = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: ".",
+        tsConfigPath: "./tsconfig.path.json",
+      },
+    },
+  ],
+};
