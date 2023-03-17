bf771d47842a9d1542d6bb00f7494d89f308ecf3
Iryna <iryna.burianovata@gmail.com>
2023-03-17 13:10:57 +0200

    Add FullWidthCarousel

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,3 +1,28 @@
+import { FullWidthCarousel } from "@/components/fullWidthCarousel/FullWidthCarousel";
+
+const slides = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Spring Sale",
+    link: "/sale",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1612731486606-2614b4d74921?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80",
+    title: "Spring Sale",
+    link: "/sale",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1538330627166-33d1908c210d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  },
+];
+
 export const Home = (): React.ReactElement => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <FullWidthCarousel slides={slides} />
+    </div>
+  );
 };
