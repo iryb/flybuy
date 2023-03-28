f0acc930d05efdbbe6786819ebca1e51df8fe49d
Iryna <iryna.burianovata@gmail.com>
2023-03-28 17:40:53 +0300

    Fix TS error and improve code structure

---
+++ client/src/common/validationSchemas/schemas.ts
@@ -0,0 +1,100 @@
+import * as yup from "yup";
+import { CheckoutSchemaValues } from "@/common/types/types";
+
+export const checkoutInitialValues: CheckoutSchemaValues = {
+  billingAddress: {
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  shippingAddress: {
+    isSameAddress: true,
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  email: "",
+  phoneNumber: "",
+};
+
+export const checkoutSchema = [
+  yup.object().shape({
+    billingAddress: yup.object().shape({
+      firstName: yup.string().required("required"),
+      lastName: yup.string().required("required"),
+      country: yup.string().required("required"),
+      street1: yup.string().required("required"),
+      street2: yup.string(),
+      city: yup.string().required("required"),
+      state: yup.string().required("required"),
+      zipCode: yup.string().required("required"),
+    }),
+    shippingAddress: yup.object().shape({
+      isSameAddress: yup.boolean().required("required"),
+      firstName: yup.string().when("isSameAddress", {
+        is: false,
+        then: () =>
+          yup.object({
+            firstName: yup.string().required("required"),
+          }),
+      }),
+      lastName: yup.string().when("isSameAddress", {
+        is: false,
+        then: () =>
+          yup.object({
+            lastName: yup.string().required("required"),
+          }),
+      }),
+      country: yup.string().when("isSameAddress", {
+        is: false,
+        then: () =>
+          yup.object({
+            country: yup.string().required("required"),
+          }),
+      }),
+      street1: yup.string().when("isSameAddress", {
+        is: false,
+        then: () =>
+          yup.object({
+            street1: yup.string().required("required"),
+          }),
+      }),
+      street2: yup.string(),
+      city: yup.string().when("isSameAddress", {
+        is: false,
+        then: () =>
+          yup.object({
+            street2: yup.string().required("required"),
+          }),
+      }),
+      state: yup.string().when("isSameAddress", {
+        is: false,
+        then: () =>
+          yup.object({
+            state: yup.string().required("required"),
+          }),
+      }),
+      zipCode: yup.string().when("isSameAddress", {
+        is: false,
+        then: () =>
+          yup.object({
+            zipCode: yup.string().required("required"),
+          }),
+      }),
+    }),
+  }),
+  yup.object().shape({
+    email: yup.string().required("required"),
+    phoneNumber: yup.string().required("required"),
+  }),
+];
--- client/src/pages/checkout/Checkout.tsx
+++ client/src/pages/checkout/Checkout.tsx
@@ -1,104 +1,15 @@
 import React, { useState } from "react";
 import { Formik } from "formik";
-import * as yup from "yup";
 import { useAppSelector } from "@/store/hooks";
 import { Box, Container } from "@mui/system";
 import { Step, StepLabel, Stepper, Typography } from "@mui/material";
 import { Shipping } from "./Shipping";
-import { CheckoutSchemaValues } from "@/common/types/types";
+import {
+  checkoutInitialValues,
+  checkoutSchema,
+} from "@/common/validationSchemas/schemas";
 
 import styles from "./styles.module.scss";
-
-const initialValues: CheckoutSchemaValues = {
-  billingAddress: {
-    firstName: "",
-    lastName: "",
-    country: "",
-    street1: "",
-    street2: "",
-    city: "",
-    state: "",
-    zipCode: "",
-  },
-  shippingAddress: {
-    isSameAddress: true,
-    firstName: "",
-    lastName: "",
-    country: "",
-    street1: "",
-    street2: "",
-    city: "",
-    state: "",
-    zipCode: "",
-  },
-  email: "",
-  phoneNumber: "",
-};
-
-const checkoutSchema = [
-  yup.object().shape({
-    billingAddress: yup.object().shape({
-      firstName: yup.string().required("required"),
-      lastName: yup.string().required("required"),
-      country: yup.string().required("required"),
-      street1: yup.string().required("required"),
-      street2: yup.string(),
-      city: yup.string().required("required"),
-      state: yup.string().required("required"),
-      zipCode: yup.string().required("required"),
-    }),
-    shippingAddress: yup.object().shape({
-      isSameAddress: yup.boolean().required("required"),
-      firstName: yup.string().when("isSameAddress", {
-        // @ts-expect-error
-        is: false,
-        then: yup.string().required("required"),
-        otherwise: "",
-      }),
-      lastName: yup.string().when("isSameAddress", {
-        // @ts-expect-error
-        is: false,
-        then: yup.string().required("required"),
-        otherwise: "",
-      }),
-      country: yup.string().when("isSameAddress", {
-        // @ts-expect-error
-        is: false,
-        then: yup.string().required("required"),
-        otherwise: "",
-      }),
-      street1: yup.string().when("isSameAddress", {
-        // @ts-expect-error
-        is: false,
-        then: yup.string().required("required"),
-        otherwise: "",
-      }),
-      street2: yup.string(),
-      city: yup.string().when("isSameAddress", {
-        // @ts-expect-error
-        is: false,
-        then: yup.string().required("required"),
-        otherwise: "",
-      }),
-      state: yup.string().when("isSameAddress", {
-        // @ts-expect-error
-        is: false,
-        then: yup.string().required("required"),
-        otherwise: "",
-      }),
-      zipCode: yup.string().when("isSameAddress", {
-        // @ts-expect-error
-        is: false,
-        then: yup.string().required("required"),
-        otherwise: "",
-      }),
-    }),
-  }),
-  yup.object().shape({
-    email: yup.string().required("required"),
-    phoneNumber: yup.string().required("required"),
-  }),
-];
 
 export const Checkout = (): React.ReactElement => {
   const [activeStep, setActiveStep] = useState(0);
@@ -127,7 +38,7 @@ export const Checkout = (): React.ReactElement => {
         <Box className={styles.pageContent}>
           <Formik
             onSubmit={handleFormSubmit}
-            initialValues={initialValues}
+            initialValues={checkoutInitialValues}
             validationSchema={checkoutSchema[activeStep]}
           >
             {({
