f8cca4dd6d18b9a7cbbba854260970a95f5641a9
Iryna <iryna.burianovata@gmail.com>
2023-04-06 17:55:36 +0300

    Add unique size helper

--- client/src/helpers/helpers.ts
+++ client/src/helpers/helpers.ts
@@ -1,3 +1,5 @@
+import { CartItem } from "@/common/types/types";
+
 export const formatPrice = (num: number): string => {
   return num.toLocaleString("en-US", { style: "currency", currency: "USD" });
 };
@@ -6,4 +8,15 @@ export const formatStringCapitalize = (str: string): string => {
   return str
     .replace(/([A-Z])/g, " $1")
     .replace(/^./g, (word) => word.toUpperCase());
+};
+
+export const getUniqueSizes = (items: CartItem[]): string[] => {
+  const sizes = items.map((item) => item.attributes.size?.data);
+  const uniqueSizes = new Set<string>();
+
+  sizes.forEach((size) => {
+    size?.forEach((s) => (s.count > 0 ? uniqueSizes.add(s.size) : null));
+  });
+
+  return Array.from(uniqueSizes);
 };
