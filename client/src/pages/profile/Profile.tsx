4013237a59a619d4ea6279539a32dfcb0fbd22c2
Iryna <iryna.burianovata@gmail.com>
2023-05-11 19:24:48 +0300

    Add Profile page

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Box, Container, Typography } from "@mui/material";
+import { useAppSelector, useAppDispatch } from "@/store/hooks";
+import clsx from "clsx";
+import { Navigate } from "react-router-dom";
+import { fetchOrders } from "@/store/order/slice";
+
+import styles from "./styles.module.scss";
 
 export const Profile = (): React.ReactElement => {
-  return <div>Profile</div>;
+  const user = useAppSelector((state) => state.user.data);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    dispatch(fetchOrders(user.id));
+  }, []);
+
+  if (!user.name) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Box className={clsx("section", styles.pageContent)}>
+      <Container>
+        <Typography
+          variant="h1"
+          className={clsx("pageTitle", styles.pageTitle)}
+        >
+          Welcome back, {user.name}!
+        </Typography>
+        <Typography variant="h3" className={styles.ordersTitle}>
+          Your Orders
+        </Typography>
+      </Container>
+    </Box>
+  );
 };
