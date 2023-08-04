97076224f7e14468204d1db50ae0aa1ebac5ad5c
Iryna <iryna.burianovata@gmail.com>
2023-08-04 17:50:05 +0300

    Add pagination

--- client/src/pages/category/Category.tsx
+++ client/src/pages/category/Category.tsx
@@ -1,13 +1,19 @@
-import { Box, Container, Typography, Grid, Chip } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  Grid,
+  Chip,
+  Pagination,
+} from "@mui/material";
 import { useParams, useSearchParams } from "react-router-dom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ApiPath } from "@enums/apiPath";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { setItems, setSizes, setPriceRange } from "@store/cart/slice";
 import { ProductCard } from "@components/productCard/ProductCard";
 import { v4 as uuidv4 } from "uuid";
 import { Filters } from "@/components/filters/Filters";
-import { getUniqueSizes, getPriceRange } from "@helpers/helpers";
 
 import styles from "./styles.module.scss";
 
@@ -16,13 +22,15 @@ export const Category = (): React.ReactElement => {
   const items = useAppSelector((state) => state.cart.items);
   const dispatch = useAppDispatch();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
 
   const querySizes = searchParams.getAll("size");
   const maxPrice = searchParams.get("maxPrice");
   const querySubcategories = searchParams.getAll("subcat");
 
   const queryParams = new URL(
-    `${ApiPath.ROOT}/api/items?filters[personCategory][$eq]=${slug}&populate=image`,
+    `${ApiPath.ROOT}/api/items?filters[personCategory][$eq]=${slug}&populate=image&pagination[page]=${page}&pagination[pageSize]=9`,
   );
 
   querySizes?.forEach((s) => {
@@ -46,25 +54,18 @@ export const Category = (): React.ReactElement => {
     });
     const filteredItemsData = await filteredItems.json();
     dispatch(setItems(filteredItemsData.data));
-
-    const items = await fetch(
-      `${ApiPath.ROOT}/api/items?filters[personCategory][$eq]=${slug}&populate=image`,
-      {
-        method: "GET",
-      },
-    );
-    const itemsData = await items.json();
-    const sizes = getUniqueSizes(itemsData.data);
-    dispatch(setSizes(sizes));
-    const priceRange = getPriceRange(itemsData.data);
-    dispatch(setPriceRange(priceRange));
-    return itemsData;
+    setPageCount(filteredItemsData.meta.pagination.pageCount);
+    return filteredItemsData;
   }
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     getItems();
-  }, [slug, searchParams]);
+  }, [slug, searchParams, page]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [searchParams]);
 
   const handleRemoveSizeFilter = (s: string): void => {
     const params: {
@@ -124,6 +125,13 @@ export const Category = (): React.ReactElement => {
     setSearchParams(params);
   };
 
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ): void => {
+    setPage(value);
+  };
+
   return (
     <Box className={styles.pageContent}>
       <Container>
@@ -163,6 +171,15 @@ export const Category = (): React.ReactElement => {
                 </Grid>
               ))}
             </Grid>
+            {pageCount > 1 && (
+              <Grid container mt={4} className={styles.paginationContainer}>
+                <Pagination
+                  count={pageCount}
+                  page={page}
+                  onChange={handlePageChange}
+                />
+              </Grid>
+            )}
           </Grid>
         </Grid>
       </Container>
