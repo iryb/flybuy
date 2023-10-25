42a530985f43e9c80838c15e79b534cbd6b02c83
Iryna <iryna.burianovata@gmail.com>
2023-10-25 18:54:42 +0300

    Add loading Skeleton

--- client/src/components/categoriesBlocks/CategoriesBlocks.tsx
+++ client/src/components/categoriesBlocks/CategoriesBlocks.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Skeleton } from "@mui/material";
 import { ApiPath, ROOT } from "@enums/apiPath";
 import { getProductImage } from "@helpers/helpers";
 import { Link } from "react-router-dom";
 import { useFetch } from "@/hooks/hooks";
 import { Category } from "@/common/types/types";
-import { Loader } from "@components/general/loader/Loader";
+import { Image } from "@components/general/image/Image";
 
 import styles from "./styles.module.scss";
 
@@ -14,33 +14,33 @@ export const CategoriesBlocks = (): React.ReactElement => {
     `${ROOT}${ApiPath.CATEGORIESAPI}?populate=image`,
   );
 
-  if (loading) return <Loader />;
-
   return (
-    <>
-      {data && (
-        <Box className={styles.section}>
-          <Grid container>
-            {data.map((category: Category, index: number) => (
-              <Grid item sm={4} key={index} className={styles.category}>
-                <Link
-                  to={`category${category.attributes.link}`}
-                  className={styles.link}
-                ></Link>
-                <Box className={styles.imageContainer}>
-                  <img
-                    src={getProductImage(category.attributes.image)}
-                    alt={category.attributes.title}
-                  />
-                </Box>
-                <Typography variant="h3" className={styles.categoryTitle}>
-                  {category.attributes.title}
-                </Typography>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-      )}
-    </>
+    <Box className={styles.section}>
+      <Grid container>
+        {loading ? (
+          <Skeleton variant="rectangular" width={"100%"} height={600} />
+        ) : (
+          data.map((category: Category, index: number) => (
+            <Grid item sm={4} key={index} className={styles.category}>
+              <Link
+                to={`category${category.attributes.link}`}
+                className={styles.link}
+              ></Link>
+              <Box className={styles.imageContainer}>
+                <Image
+                  src={getProductImage(category.attributes.image)}
+                  alt={category.attributes.title}
+                  width="640px"
+                  height="600px"
+                />
+              </Box>
+              <Typography variant="h3" className={styles.categoryTitle}>
+                {category.attributes.title}
+              </Typography>
+            </Grid>
+          ))
+        )}
+      </Grid>
+    </Box>
   );
 };
